b82d686804991926a6f60fe56b79e0bb390e14bf
axel philibert <axelphilibert@outlook.fr>
2021-02-04 21:23:19 +0100

    New Connexion ajoutée

--- app/src/main/java/fr/esme/esme_map/ui/main/ConnexionFragment.kt
+++ app/src/main/java/fr/esme/esme_map/ui/main/ConnexionFragment.kt
@@ -8,9 +8,16 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.gson.Gson
 import fr.esme.esme_map.MainActivity
 import fr.esme.esme_map.MapActivity
 import fr.esme.esme_map.R
+import fr.esme.esme_map.model.User
+import fr.esme.esme_map.repository.UserRepository
+import kotlin.concurrent.thread
 
 class ConnexionFragment(private val context: MainActivity) : Fragment() {
 
@@ -35,14 +42,31 @@ class ConnexionFragment(private val context: MainActivity) : Fragment() {
 
         activity?.findViewById<Button>(R.id.button_connexion)?.setOnClickListener {
 
-            //redirection vers la map
-            val myIntent = Intent(context, MapActivity::class.java)
-            startActivityForResult(myIntent, MAP_ACTIVITY)
 
-            //Vérification de l'user dans la base de donnée
-            if (UserText?.text.toString() == "" ){
+            UserRepository.Singleton.databaseUserRef.addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
 
-            }
+                    //récolter la liste
+                    for (ds in snapshot.children) {
+
+                        System.out.println(ds.child("username").value)
+                        //Vérification de l'user dans la base de donnée
+                        if (UserText?.text.toString() == ds.child("username").value ){
+
+                            //redirection vers la map
+                            val myIntent = Intent(context, MapActivity::class.java)
+                            startActivityForResult(myIntent, MAP_ACTIVITY)
+                        }
+
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+            })
+
+
+
 
             System.out.println(UserText?.text.toString())
 
